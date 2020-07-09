0902236d3ce54a950d0a77279c75b18f4f9aaa1c
SettlementApp <aki01191017@gmail.com>
2020-07-10 06:00:10 +0900

    Initial Commit

---
+++ firebaseApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  firebaseApp
+//
+//  Created by 柿沼儀揚 on 2020/07/10.
+//  Copyright © 2020 柿沼儀揚. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
